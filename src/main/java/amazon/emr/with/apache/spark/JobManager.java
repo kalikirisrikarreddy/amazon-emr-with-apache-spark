0ba0ef04041891daf638fd09c8fa6a90dcbeb9ce
Srikar Reddy Kalikiri <kalikirisrikarreddy@gmail.com>
2023-08-30 12:02:21 +0530

    added code to create an EMR cluster and run the steps

--- src/main/java/amazon/emr/with/apache/spark/JobManager.java
+++ src/main/java/amazon/emr/with/apache/spark/JobManager.java
@@ -5,86 +5,60 @@ import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.emr.EmrClient;
 import software.amazon.awssdk.services.emr.model.ActionOnFailure;
+import software.amazon.awssdk.services.emr.model.AddJobFlowStepsRequest;
 import software.amazon.awssdk.services.emr.model.Application;
 import software.amazon.awssdk.services.emr.model.HadoopJarStepConfig;
 import software.amazon.awssdk.services.emr.model.JobFlowInstancesConfig;
+import software.amazon.awssdk.services.emr.model.PlacementType;
 import software.amazon.awssdk.services.emr.model.RunJobFlowRequest;
+import software.amazon.awssdk.services.emr.model.RunJobFlowResponse;
 import software.amazon.awssdk.services.emr.model.StepConfig;
-import software.amazon.awssdk.services.iam.IamClient;
-import software.amazon.awssdk.services.iam.model.AttachRolePolicyRequest;
-import software.amazon.awssdk.services.iam.model.CreateRoleRequest;
 
 public class JobManager {
-
-	static IamClient iamClient = IamClient.builder().region(Region.US_EAST_1)
-			.credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(null, null))).build();
 
 	static EmrClient emrClient = EmrClient.builder().region(Region.US_EAST_1)
 			.credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(null, null))).build();
 
 	public static void main(String[] args) {
-		createNecessaryServiceRoles();
-		Application spark = Application.builder().name("Spark").build();
-		RunJobFlowRequest request = RunJobFlowRequest.builder().name("My EMR Cluster").releaseLabel("emr-6.0.0")
-				.steps(StepConfig.builder().name("Spark Word Count").actionOnFailure(ActionOnFailure.TERMINATE_CLUSTER)
-						.hadoopJarStep(HadoopJarStepConfig.builder()
-								.jar("s3://apache-spark-sample-code-artifacts-bucket-1/apache-spark-sample.jar")
-								.mainClass("apache.spark.SparkSample.java").build())
-						.build())
-				.applications(spark).logUri("s3://emr-with-apache-spark-logs-1").serviceRole("EMRRole")
-				.jobFlowRole("EMREC2Role")
-				.instances(JobFlowInstancesConfig.builder().ec2SubnetId("subnet-05b221f1672c794d7").ec2KeyName("kd1kp")
-						.instanceCount(1).keepJobFlowAliveWhenNoSteps(false).masterInstanceType("m4.large").build())
-				.build();
-		emrClient.runJobFlow(request);
+		createClusterAndRunSparkJob();
 	}
 
-	private static void createNecessaryServiceRoles() {
-		// This role will be assumed by EMR service to access other AWS services on our
-		// behalf.
-		iamClient.createRole(CreateRoleRequest.builder().roleName("EMRRole").assumeRolePolicyDocument("""
-				{
-				    "Version": "2012-10-17",
-				    "Statement": [
-				        {
-				            "Effect": "Allow",
-				            "Action": [
-				                "sts:AssumeRole"
-				            ],
-				            "Principal": {
-				                "Service": [
-				                    "elasticmapreduce.amazonaws.com"
-				                ]
-				            }
-				        }
-				    ]
-				}
-				""").build());
-		iamClient.attachRolePolicy(AttachRolePolicyRequest.builder()
-				.policyArn("arn:aws:iam::aws:policy/service-role/AmazonElasticMapReduceRole").build());
+	private static void createClusterAndRunSparkJob() {
+		Application spark = Application.builder().name("Spark").build();
+		Application hadoop = Application.builder().name("Hadoop").build();
+		Application zeppelin = Application.builder().name("Zeppelin").build();
 
-		// This role will be part of the ec2 instance profile, so that nodes in the EMR
-		// cluster have needed access.
-		iamClient.createRole(CreateRoleRequest.builder().roleName("EMREC2Role").assumeRolePolicyDocument("""
-				{
-				    "Version": "2012-10-17",
-				    "Statement": [
-				        {
-				            "Effect": "Allow",
-				            "Action": [
-				                "sts:AssumeRole"
-				            ],
-				            "Principal": {
-				                "Service": [
-				                    "ec2.amazonaws.com"
-				                ]
-				            }
-				        }
-				    ]
-				}
-								""").build());
-		iamClient.attachRolePolicy(AttachRolePolicyRequest.builder()
-				.policyArn("arn:aws:iam::aws:policy/service-role/AmazonElasticMapReduceforEC2Role").build());
+		StepConfig downloadingJarFromS3 = StepConfig.builder().name("Download Jar From S3 To EMR Cluster")
+				.actionOnFailure(ActionOnFailure.TERMINATE_CLUSTER)
+				.hadoopJarStep(HadoopJarStepConfig.builder().jar("command-runner.jar")
+						.args("aws", "s3api", "get-object", "--bucket", "apache-spark-sample-code-artifacts-bucket-1",
+								"--key", "apache-spark-sample-1.0.0-jar-with-dependencies.jar",
+								"/home/hadoop/apache-spark-sample-1.0.0-jar-with-dependencies.jar")
+						.build())
+				.build();
+
+		StepConfig workCountInSpark = StepConfig.builder().name("Spark Word Count")
+				.actionOnFailure(ActionOnFailure.TERMINATE_CLUSTER)
+				.hadoopJarStep(HadoopJarStepConfig.builder().jar("command-runner.jar")
+						.args("spark-submit", "--deploy-mode", "client",
+								"file:///home/hadoop/apache-spark-sample-1.0.0-jar-with-dependencies.jar")
+						.build())
+				.build();
+
+		RunJobFlowRequest request = RunJobFlowRequest.builder().name("My EMR Cluster").releaseLabel("emr-6.12.0")
+				.applications(spark, hadoop, zeppelin).logUri("s3://emr-with-apache-spark-logs-1")
+				.serviceRole("EMR_DefaultRole").jobFlowRole("EMR_EC2_DefaultRole")
+				.instances(JobFlowInstancesConfig.builder().ec2KeyName("kd1kp").instanceCount(1)
+						.placement(PlacementType.builder().availabilityZones("us-east-1a").build())
+						.keepJobFlowAliveWhenNoSteps(true).masterInstanceType("m4.large").build())
+				.build();
+
+		RunJobFlowResponse runJobFlowResponse = emrClient.runJobFlow(request);
+
+		emrClient.addJobFlowSteps(AddJobFlowStepsRequest.builder().jobFlowId(runJobFlowResponse.jobFlowId())
+				.steps(downloadingJarFromS3).build());
+		emrClient.addJobFlowSteps(AddJobFlowStepsRequest.builder().jobFlowId(runJobFlowResponse.jobFlowId())
+				.steps(workCountInSpark).build());
 	}
 
 }
